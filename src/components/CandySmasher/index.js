fb87ee7c96dff4b96e9d426b4ab27eecff5628b2
chaz Graham <chazhg80.cg@gmail.com>
2023-07-19 19:08:26 -0500

    added check for collums of 4

--- src/components/CandySmasher/index.js
+++ src/components/CandySmasher/index.js
@@ -5,9 +5,9 @@ function CandySmasher() {
     const [currentColorArr, setCurrentColorArr] = useState([]);
 
     const width = 8;
-    const candyColorArr = ['red', 'blue', 'green', 'yellow', 'orange', 'purple'];
     
     useEffect(() => {
+        const candyColorArr = ['red', 'blue', 'green', 'yellow', 'orange', 'purple'];
         const createBoard = () => {
             const randomColorArr = [];
             for (let i = 0; i < width * width; i++) {
@@ -22,7 +22,7 @@ function CandySmasher() {
     const checkForCollumOfThree = () => {
         // loops through each square until last collum of 3, which happens to end at index of 47
         for (let i = 0; i < 47 ; i++) {
-            // i is the first candy in the collum, i + width is the candy right under it, and i + width is the candy under candy i + width
+            // i is the first candy in the collum, i + width is the candy right under it, and i + width * 2 is the candy under candy i + width
             const collumOfTree = [i, i + width, i + width * 2]
             // the color that all the candies should be in the collum of three
             const selectedColor = currentColorArr[i]
@@ -34,8 +34,22 @@ function CandySmasher() {
         }
     }
 
+    const checkForCollumOfFour = () => {
+        // loops through each square until last collum of 4, which happens to end at index of 39
+        for (let i = 0; i < 39 ; i++) {
+            // i is the first candy in the collum, i + width is the candy right under it, and i + width * 2 is the candy under candy i + width, i + width * 3 is under i + width * 2 
+            const collumOfFour = [i, i + width, i + width * 2, i + width * 3]
+            const selectedColor = currentColorArr[i]
+            
+            if ( collumOfFour.every(candy => currentColorArr[candy] === selectedColor)) {
+                collumOfFour.forEach(candy => currentColorArr[candy] = '')
+            }
+        }
+    }
+
     useEffect(() => {
         const timer = setInterval(() => {
+            checkForCollumOfFour()
             checkForCollumOfThree()
             setCurrentColorArr([...currentColorArr])
         }, 100);
