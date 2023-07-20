cf988342d2416c5321edbb4c4f9564228da82e16
chaz Graham <chazhg80.cg@gmail.com>
2023-07-19 19:32:47 -0500

    added remove row of 3 and 4

--- src/components/CandySmasher/index.js
+++ src/components/CandySmasher/index.js
@@ -47,10 +47,40 @@ function CandySmasher() {
         }
     }
 
+    const checkForRowOfTree = () => {
+        for (let i = 0; i < 64 ; i++) {
+            const rowOfThree = [i, i + 1, i + 2]
+            const selectedColor = currentColorArr[i]
+            const notValid =  [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+
+            if (notValid.includes(i)) continue
+            
+            if ( rowOfThree.every(candy => currentColorArr[candy] === selectedColor)) {
+                rowOfThree.forEach(candy => currentColorArr[candy] = '')
+            }
+        }
+    }
+
+    const checkForRowOfFour = () => {
+        for (let i = 0; i < 64 ; i++) {
+            const rowOfFour = [i, i + 1, i + 2, i + 3]
+            const selectedColor = currentColorArr[i]
+            const notValid =  [5, 6, 7, 13,14, 15, 21,22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+
+            if (notValid.includes(i)) continue
+            
+            if ( rowOfFour.every(candy => currentColorArr[candy] === selectedColor)) {
+                rowOfFour.forEach(candy => currentColorArr[candy] = '')
+            }
+        }
+    }
+    
     useEffect(() => {
         const timer = setInterval(() => {
             checkForCollumOfFour()
             checkForCollumOfThree()
+            checkForRowOfFour()
+            checkForRowOfTree()
             setCurrentColorArr([...currentColorArr])
         }, 100);
         return () => clearInterval(timer)
