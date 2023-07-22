02c54de9ed9b67d1cd58ad312999583d5c7bd9d6
chaz Graham <chazhg80.cg@gmail.com>
2023-07-22 14:58:52 -0500

    created 3x3 game board for ticTac with all slots rendering as white

--- src/components/TicTacToe/index.js
+++ src/components/TicTacToe/index.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function TicTacToe () {
+    const [currentPiece, setCurrentPiece] = useState([]);
+    const width = 3;
+    
+    
+    useEffect(() => {
+        const createBoard = () => {
+            const newBoard = [];
+            for (var i = 0; i < width * width; i++) {
+                newBoard.push('white')
+            }
+            setCurrentPiece(newBoard)
+        }
+        createBoard()
+    }, [])
+
     return (
         <>
-            <h1>
-                Tic-Tac-Toe
-            </h1>
+            <section className="tac_container">
+                <div className="tac_gameBoard">
+                    {currentPiece.map((move, index) => (
+                        <img
+                            alt="moveSlot"
+                            style={{backgroundColor: move}}
+                        />
+                    ))}
+                </div>
+            </section>
         </>
     )
 }
