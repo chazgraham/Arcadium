5ddc452f0421f6a8b5ba692f47a4b6f365f854a6
chaz Graham <chazhg80.cg@gmail.com>
2023-07-23 14:58:26 -0500

    added game board background image and colors for the player and bot moves

--- src/components/TicTacToe/index.js
+++ src/components/TicTacToe/index.js
@@ -3,8 +3,8 @@ import "./ticTacToe.css";
 
 function TicTacToe () {
     const [currentPiece, setCurrentPiece] = useState([]);
-    const [yourMove, setYourMove] = useState(true)
-    const [takenSpots, setTakenSpots] = useState([])
+    const [yourMove, setYourMove] = useState(true);
+    const [takenSpots, setTakenSpots] = useState([]);
     
     const width = 3;
     
@@ -12,66 +12,64 @@ function TicTacToe () {
         const createBoard = () => {
             const newBoard = [];
             for (var i = 0; i < width * width; i++) {
-                newBoard.push('')
+                newBoard.push('');
             }
-            setCurrentPiece(newBoard)
+            setCurrentPiece(newBoard);
         }
-        createBoard()
+        createBoard();
     }, [])
 
     const selectSpot = (e) => {
-        const takenSpot = [...takenSpots]
+        const takenSpot = [...takenSpots];
+ 
         if (yourMove === true) {
+            e.target.style.color = 'blue'
             const spotBeingTaken =(e.target);
-            const spotBeingTakenId = parseInt(spotBeingTaken.getAttribute("data-id"))
-            currentPiece[spotBeingTakenId] = 'X'
-            setCurrentPiece([...currentPiece])
-            takenSpot.push(spotBeingTakenId)
-            setYourMove(false)
+            const spotBeingTakenId = parseInt(spotBeingTaken.getAttribute("data-id"));
+            currentPiece[spotBeingTakenId] = 'X';
+            setCurrentPiece([...currentPiece]);
+            takenSpot.push(spotBeingTakenId);
+            setYourMove(false);
         }
-        setTakenSpots(takenSpot)
+        setTakenSpots(takenSpot);
     }
 
     const botMove = () => {
         if (takenSpots.length === 9) {
-            console.log('game over')
+            console.log('game over');
         }
 
         if (yourMove !== true) {
-            const takenSpot = [...takenSpots]
-            const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-            const avalibleSpot = nums.filter((i) => !takenSpot.includes(i))
-            const getRandomPeice = avalibleSpot[Math.floor(Math.random()*avalibleSpot.length)]
-
+            const takenSpot = [...takenSpots];
+            const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+            const avalibleSpot = nums.filter((i) => !takenSpot.includes(i));
+            const getRandomPeice = avalibleSpot[Math.floor(Math.random()*avalibleSpot.length)];
+            
             for (var i = 0; i < 1; i++) {
-                currentPiece[getRandomPeice] = 'O'
-                setCurrentPiece([...currentPiece])
-                takenSpot.push(getRandomPeice)
-                setYourMove(true)
+                currentPiece[getRandomPeice] = 'O';
+                setCurrentPiece([...currentPiece]);
+                takenSpot.push(getRandomPeice);
+                setYourMove(true);
             }
-            setTakenSpots(takenSpot)
+            setTakenSpots(takenSpot);
         }
     }
 
     useEffect(() => {
         const timer = setInterval(() => {
-            botMove()
-        }, 1000)
-        return () => clearInterval(timer)
+            botMove();
+        }, 1000);
+        return () => clearInterval(timer);
     })
-
 
     return (
         <>
             <section className="tac_container">
                 <div className="tac_gameBoard">
-                    {currentPiece.map((move, index) => (
-                        <p
-                            key={index}
-                            alt="moveSlot"
-                            data-id={index}
-                            onClick={selectSpot}
-                        >{move}</p>
+                    {currentPiece.map((move, index, color) => (
+                        <p key={index} alt="moveSlot" data-id={index} onClick={selectSpot} style={{color: 'red'}}>
+                            {move}
+                        </p>
                     ))}
                 </div>
             </section>
