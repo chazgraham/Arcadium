9f7b9e21b1f708dea165222b9b675af8c9bcc2ba
chaz Graham <chazhg80.cg@gmail.com>
2023-07-26 15:15:30 -0500

    added RockPaperScissors component with index.js file

---
+++ src/components/RockPaperScissors/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RockPaperScissors() {
+    return (
+        <>
+            <h2>Rock Paper Scissors</h2>
+        </>
+    )
+}
+
+export default RockPaperScissors;
