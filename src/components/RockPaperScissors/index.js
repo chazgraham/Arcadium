f110348d13da79f9bd8518c4b2c0d3697b3e2205
chaz Graham <chazhg80.cg@gmail.com>
2023-07-30 16:55:50 -0500

    imported rps images and now render for bot and player choices

--- src/components/RockPaperScissors/index.js
+++ src/components/RockPaperScissors/index.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import './rockPaperScissors.css';
 
+import rock from '../../assets/images/rpsImgs/rock.png';
+import paper from '../../assets/images/rpsImgs/paper.png';
+import playerScissors from '../../assets/images/rpsImgs/playerScissors.png';
+import botScissors from '../../assets/images/rpsImgs/botScissors.png';
+
 function RockPaperScissors() {
     const [playerChoice, setPlayerChoice] = useState('')
     const [botChoice, setBotChoice] = useState('')
     console.log(playerChoice)
     console.log(botChoice)
 
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [rock, paper, botScissors]
     const botChoices = () => {
         const getRandomChoice = choices[Math.floor(Math.random() * choices.length)];
         setBotChoice(getRandomChoice)
@@ -18,16 +23,16 @@ function RockPaperScissors() {
             <section className="rps_container">
                 <div className="player_container">
                     <div>
-                        <img className="rps_img" alt="player Choice" />
+                        <img className="rps_img" alt="player Choice" src={playerChoice} />
                     </div>
                     <div>
-                        <button onClick={() => { setPlayerChoice('rock'); botChoices() }}>Rock</button>
-                        <button onClick={() => { setPlayerChoice('paper'); botChoices() }}>Paper</button>
-                        <button onClick={() => { setPlayerChoice('scissors'); botChoices() }}>Scissors</button>
+                        <button onClick={() => { setPlayerChoice(rock); botChoices() }}>Rock</button>
+                        <button onClick={() => { setPlayerChoice(paper); botChoices() }}>Paper</button>
+                        <button onClick={() => { setPlayerChoice(playerScissors); botChoices() }}>Scissors</button>
                     </div>
                 </div>
                 <div>
-                    <img className="rps_img" alt="bot choice" />
+                    <img className="rps_img" alt="bot choice" src={botChoice} />
                 </div>
             </section>
         </>
