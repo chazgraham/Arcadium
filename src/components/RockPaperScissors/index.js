5ab0d0cdde2eba2ab08b8a7650dfa8c6085b9d36
chaz Graham <chazhg80.cg@gmail.com>
2023-07-30 16:25:35 -0500

    bot gets a random choice from choices array

--- src/components/RockPaperScissors/index.js
+++ src/components/RockPaperScissors/index.js
@@ -2,16 +2,24 @@ import React, { useState } from "react";
 
 function RockPaperScissors() {
     const [playerChoice, setPlayerChoice] = useState('')
+    const [botChoice, setBotChoice] = useState('')
     console.log(playerChoice)
+    console.log(botChoice)
+
+    const choices = ['rock', 'paper', 'scissors']
+    const botChoices = () => {
+        const getRandomChoice = choices[Math.floor(Math.random()*choices.length)];
+        setBotChoice(getRandomChoice)
+    }
 
     return (
         <>
             <section>
                 <div>
                     <img alt="player Choice" />
-                    <button onClick={ () => setPlayerChoice('rock')}>Rock</button>
-                    <button onClick={ ()=> setPlayerChoice('paper')}>Paper</button>
-                    <button onClick={ ()=> setPlayerChoice('scissors')}>Scissors</button>
+                    <button onClick={ () => {setPlayerChoice('rock'); botChoices()}}>Rock</button>
+                    <button onClick={ ()=> {setPlayerChoice('paper'); botChoices()}}>Paper</button>
+                    <button onClick={ ()=> {setPlayerChoice('scissors'); botChoices()}}>Scissors</button>
                 </div>
             </section>
         </>
